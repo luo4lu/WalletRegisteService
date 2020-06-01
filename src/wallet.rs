3c7adc2fd733161b76987d0b0e76fa84ffbbe2ed
luo4lu <luo4lu@163.com>
2020-06-01 18:58:18 +0800

    add get wallets info function interface

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,9 +1,10 @@
 use crate::response::ResponseBody;
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{post, get, web, HttpResponse, Responder};
 use asymmetric_crypto::hasher::sm3::Sm3;
 use dislog_hal::Hasher;
 use hex::ToHex;
 use serde::{Deserialize, Serialize};
+use log::{info, warn};
 //数据库相关
 use deadpool_postgres::Pool;
 
@@ -51,3 +52,40 @@ pub async fn new_reg_wallet(
         uid: uid_str,
     })))
 }
+
+//get wallet info
+#[derive(Deserialize, Debug)]
+pub struct GetWalletRequest {
+    uid: String,
+}
+
+#[derive(Serialize)]
+pub struct GetWalletRespones {
+    cert: String,
+    info: String,
+}
+
+#[get("/api/wallet")]
+pub async fn get_wallet_info(data: web::Data<Pool>,
+    req: web::Json<GetWalletRequest>)-> impl Responder{
+        //连接数据库获取句柄
+        let conn = data.get().await.unwrap();
+        let inset_statement = match conn.query(
+                "SELECT * from wallets where id = $1", &[&req.uid]).await{
+                    Ok(row) => {
+                        info!("select success!{:?}",row);
+                        row
+                    }
+                    Err(error) => {
+                        warn!("select failde!!{:?}",error);
+                        return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+                    }
+                };
+        let v_cert = inset_statement[0].get(1);
+        let v_info:serde_json::Value = inset_statement[0].get(2);
+        let s_info = v_info.to_string();
+        HttpResponse::Ok().json(ResponseBody::new_success(Some(GetWalletRespones {
+            cert: v_cert,
+            info: s_info,
+        })))
+    } 
